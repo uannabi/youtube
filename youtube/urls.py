9bf5229afbb46d722d2da2569375876856442917
uannabi <zahid_sc@hotmail.fr>
2021-06-02 11:20:37 +0600

    django app

---
+++ youtube/urls.py
@@ -0,0 +1,25 @@
+"""youtube URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from api.fetch_youtube_data import get_channel_videos
+from api.views import YoutubeVideoList
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('channel/<str:channel_id>', get_channel_videos),
+    path('videos',YoutubeVideoList.as_view())
+]
